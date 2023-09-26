e2074d8c176368fdbb0e536482767f44f3f62303
Luis Miguel <lmavendano1597@gmail.com>
2023-09-25 23:13:20 -0500

    next commit

--- src/server.js
+++ src/server.js
@@ -1,5 +1,18 @@
+import { envs } from './config/enviroments/enviroments.js'
 import app from "./app.js"
+import { authenticate, syncUp } from './config/database/database.js'
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000")
+async function main(){
+  try {
+    await authenticate()
+    await syncUp()
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+main()
+
+app.listen(envs.PORT, () => {
+  console.log(`Server running on port ${envs.PORT}`)
 })
