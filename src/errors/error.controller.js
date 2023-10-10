e1f09f9936e548769a7f2ee3ccbe1fbdf872454a
Luis Miguel <lmavendano1597@gmail.com>
2023-10-09 22:15:06 -0500

    seguridad terminada

--- src/errors/error.controller.js
+++ src/errors/error.controller.js
@@ -7,6 +7,15 @@ const handleCastError22001 = () =>
 
 const handleCastError23505 = () =>
   new AppError('Duplicate field value: please use another value', 400)
+
+const handleCastError22P02 = () =>
+  new AppError('Invalid data type in database', 400);
+
+const handleJWTExpiredError = () => 
+  new AppError('Your token has expired! Please login again', 401)
+
+const handleJWTError = () =>
+  new AppError('Invalid token. Please login again', 401)
  
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -50,8 +59,12 @@ export const globalErrorHandler = (err, req, res, next) => {
 
   if(envs.NODE_ENV === 'production'){
     let error = err;
+ 
     if(err.parent?.code === '22001') error = handleCastError22001();
-    if(err.parent?.code === '23505') error = handleCastError23505()
+    if(err.parent?.code === '23505') error = handleCastError23505();
+    if (err.parent?.code === '22P02') error = handleCastError22P02();
+    if(err.name === 'TokenExpiredError') error = handleJWTExpiredError();
+    if(err.name === 'JsonWebTokenError') error = handleJWTError();
 
     sendErrorProd(error, res)
   }
