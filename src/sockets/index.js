eede048b90ac9ef8b3c6fe195cb86ec78f684abe
Luis Miguel <lmavendano1597@gmail.com>
2023-10-19 20:08:02 -0500

    next commit

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -1,16 +1,19 @@
+import { FlightService } from './../flights/flight.service.js'
+
 export class Socket {
   constructor(io) {
     this.io = io;
-
+    this.flightService = new FlightService()
     this.socketEvents();
   }
 
   socketEvents() {
     this.io.on('connection', (socket) => {
-      socket.on("change-status-flight", (data) => {
-        console.log(data)
+      socket.on("change-status-flight", async(data) => {
+        const flights = await this.flightService.findAllWithAllData()
+        console.log(flights)
         // socket.broadcast.emit("render-change-status", data)
-        this.io.emit("render-change-status", data)
+        this.io.emit("render-new-flights", flights)
       })
     });
   }
