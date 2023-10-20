51130f7a78aebfb37ff6976ce56346deb9382fe5
Luis Miguel <lmavendano1597@gmail.com>
2023-10-19 21:24:49 -0500

    finis app

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -1,15 +1,22 @@
-
+import { FlightService } from './../flights/flight.service.js'
 
 export class Sockets {
   constructor(io) {
     this.io = io;
-
+    this.flightService = new FlightService()
     this.socketsEvents()
   }
 
   socketsEvents(){
     this.io.on("connection", ( socket ) => {
-      console.log('Conectado al servidor!')
+      
+      socket.on("get-flights", async( { id } ) => {
+        const flights = await this.flightService.findAllWithAllData();
+        this.io.emit("news-flights", flights)
+        // socket.broadcast.emit()
+      })
+
+
     })
   }
 }
